84e0f6eaec2531b37c2e53a21676db9128e00037
Gustavo Pateti <gpateti@gmail.com>
2025-03-01 12:14:32 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <HelloWorld msg="Welcome to Magnate - Web" />
+</template>
+
+<style scoped></style>
